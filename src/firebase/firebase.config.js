c8f3614d27ea0a38f76999dab3a5cb25776a3c8d
AntarProgrammer <antarmitra33@gmail.com>
2023-10-19 01:27:33 +0600

    firebase install in website

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCuhH9pvti7Q-dBJUq3QlxvA5EJrGKjxfk",
+  authDomain: "car-shop-7dca5.firebaseapp.com",
+  projectId: "car-shop-7dca5",
+  storageBucket: "car-shop-7dca5.appspot.com",
+  messagingSenderId: "984048864779",
+  appId: "1:984048864779:web:6c05c8c2e1047a7318010e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
